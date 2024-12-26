61baebf1a400708a872834db7da0340776b75208
Ritick456 <ritickkr937@gmail.com>
2024-12-26 10:24:24 +0530

    UC3-completed

--- linecomparison.cpp
+++ linecomparison.cpp
@@ -2,6 +2,29 @@
 #include <cmath> 
 using namespace std;
 
+void compare(int x1, int y1, int x2, int y2, int x3,  int y3, int x4, int y4){
+
+    int lengthofline1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    int lengthofline2 = sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2));
+
+    cout << "The length of line1 is " << " " << lengthofline1 << endl;
+    cout << "The length of line2 is " << " " << lengthofline2 << endl;
+
+    if(lengthofline1 == lengthofline2){
+        cout << "The lines are equal" << endl;
+    }
+    else if(lengthofline1 > lengthofline2){
+        cout << "The length of line 1 is greater than length of line2" << endl;
+    }
+    else if(lengthofline2 > lengthofline1){
+        cout << "The length of line 1 is greater than length of line2" << endl;
+    }
+    else{
+        cout << "The lines are not equal" << endl;
+    }
+
+}
+
 int main() {
     cout << "Welcome to Line comparison computation program on master branch" << endl;
     int x1, y1;
@@ -15,18 +38,8 @@ int main() {
     cin>>x1>>y1>>x2>>y2;
     cin>>x3>>y3>>x4>>y4;
 
-    int lengthofline1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    int lengthofline2 = sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2));
-
-    cout << "The length of line1 is " << " " << lengthofline1 << endl;
-    cout << "The length of line2 is " << " " << lengthofline2 << endl;
-
-    if(lengthofline1 == lengthofline2){
-        cout << "The lines are equal" << endl;
-    }
-    else{
-        cout << "The lines are not equal" << endl;
-    }
+    compare(x1 , y1, x2, y2, x3, y3, x4, y4);
+   
     
     return 0;
 }
